09b38623a70cb362af426a6dc11e08d5b3181d04
IPA325 <you@example.com>
2017-11-17 15:53:48 +0100

    refresh2

--- zusamengefasst.m
+++ zusamengefasst.m
@@ -1,7 +1,12 @@
-n = 2;
+% ___________Define variables
+
+n = 3;
+
+# x
+x = sdpvar(n,1);
+
 # b: input vector
 b = [0; 0;  1];
-# k_v: position control vector
 
 # I: I of n dimension
 I = eye(n);
@@ -9,10 +14,7 @@ I = eye(n);
 M =  diag(1: n-1);
 
 # N = diag(-n,..., -2,-1)
-N = diag(-n,-1);
-
-# P_(l,k)
-#P_lk = @(k) P(l, k) .* (k != 0) + I .* (k == 0); 
+N = diag(-n:-1);
 
 % define Q
 Q = sdpvar(n,n);                
@@ -21,16 +23,26 @@ Q = sdpvar(n,n);
 A = [0 ,  1,  0;
       0,  0,  1;
       0,  0,  -0.005];
+      
+# u_max
+u_max = 2.5 * 10^(-5)
+
+# m: size of a_head
+m = 3;
+
+# a
+a = sdpvar(n,1);
+#a = [4.4469*10^-8;  2.3073*10^-5; 4.9148*10^-3];
 
 # a_head: closed loop controled coefficients
-a_head = a - b*k_v;
+a_head = [4.4469*10^-8;  2.3073*10^-5; 4.9148*10^-3];
 
 # a: route coefficients
 
 # z
 z = Q*a_head;
 
-% Q is positivedifinited
+% ___________Define constraints
 % 4.59
 F = [Q >0];
 
@@ -44,7 +56,13 @@ F = [F, Q*N + N*Q < 0];
 F = [F, [1, (x.'); x, Q] >0];
 
 % 4.63
-F = [F, [u_max^^2 -(a.')*Q*a + 2 .* (a.')*z, (z.') ; z, Q] >=0];
+#disp(size(u_max^2 -(a.')*Q*a + 2 .* (a.')*z));
+#disp(size(z.'));
+#disp(size(z));
+#disp(size(Q));
+#disp(size([(u_max^2 -(a.')*Q*a + 2 * (a.')*z), (z.')]));
+#disp(size( [ z, Q]));
+F = [F,  [[(u_max^2 -(a.')*Q*a + 2 * (a.')*z), (z.')]; [ z, Q]] >=0];
 % 4.64
 # i: belong to {0,1, ..., m}
 for i = 0 : m
@@ -56,7 +74,7 @@ function retval = P (l,k)
   for q = (0: k-1)
     retval = retval * ((l-q)*I + M);
   endfor
-  return retval;
+  return
 endfunction
 
 function retval = sum(i)
@@ -64,6 +82,11 @@ function retval = sum(i)
   for k = (0: i)
     retval = retval + nchoosek(i,k)*(a.')*P(0, i-k)*Q*N*P(0,k)*a-(z.')*N*P(n,i)*a;
   endfor
-  return retval;
+  return
 endfunction
-sdpvar
+
+% ___________Define an objective
+objective = x;
+
+% ___________optimize
+optimize(F,objective)
