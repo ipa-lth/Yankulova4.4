168cb549ccdbd1317f3b3beb5d08874fbfa0799b
IPA325 <you@example.com>
2017-11-21 13:05:41 +0100

    valid constraints

--- zusamengefasst.m
+++ zusamengefasst.m
@@ -1,4 +1,18 @@
-% ___________Define variables
+# loading path
+addpath ("/home/ipa325/Downloads/YALMIP-master")
+addpath ("/home/ipa325/Downloads/YALMIP-master/extras")
+addpath ("/home/ipa325/Downloads/YALMIP-master/solvers")
+addpath ("/home/ipa325/Downloads/YALMIP-master/modules")
+addpath ("/home/ipa325/Downloads/YALMIP-master/modules/parametric")
+addpath ("/home/ipa325/Downloads/YALMIP-master/modules/global")
+addpath ("/home/ipa325/Downloads/YALMIP-master/modules")
+addpath ("/home/ipa325/Downloads/YALMIP-master/modules/sos")
+addpath ("/home/ipa325/Downloads/YALMIP-master/operators")
+
+
+# final aim is to find the Q and z. A, b should be in canonical form 
+# ("Steuerung normal form")
+# ___________Define variables
 # number of dimention
 n   = 3;
 
@@ -7,9 +21,25 @@ Q       = sdpvar(n,n);        # Q = R^(-1)
 # a_head: closed loop controled coefficients
 # a_head  = sdpvar(n,1); #(a_head is the result!!!)
 z       = sdpvar(n,1); 
+#z       = [1; 1; 1]; 
 
-# x: input matrix (could be e.g. initial position, velocity, ect.), right?
-x       = sdpvar(n,1);
+# x: one line of Xi_o: initial state of the system
+Xi_o    = [ 10,  0.05, 0.0046;
+            10,  0.05, -0.0046;
+            10,  -0.05, 0.0046;
+            10,  -0.05, -0.0046;
+            -10,  0.05, 0.0046;
+            -10,  0.05, -0.0046;
+            -10,  -0.05, 0.0046;
+            -10,  -0.05, -0.0046;
+            ]
+            
+x       = Xi_o(1,:).';
+disp("x = ");
+disp(x);
+disp("Xi_o NoR = ");
+Xi_o_num = size (Xi_o, 1);
+disp(Xi_o_num);
 
 # b: input vector, what does it means?
 b   = [0; 0;  1];
@@ -25,20 +55,19 @@ M   =  diag(0: n-1);
 
             
 
-# A current system
+# A: current system matrix in canonical form ("Steuerung normal form")
 A = [0 ,  1,  0;
       0,  0,  1;
       0,  0,  -0.005];
       
+# a is the last line of canonical form of A
+A_nol = size(A,1);      # number of line of A
+disp ("a = ");
+a = A (A_nol,:).';     #I calculate from A, is that ok? => sure, a = last line of A
+disp (a);
+      
 # u_max
 u_max = 2.5 * 10^(-5)
-
-# m: size of a: dimention of A (nxn) ????
-m = 3;
-
-# a, A-lamda*I = 0; lamda = (a0, a1, ..., an). If we repleace A(current system)
-# by A* (system we want), we have a* as a result wanted eigen vector
-a = [40000; -200; 1];     #I calculate from A, is that ok?
 
 # a: route coefficients
 
@@ -57,7 +86,11 @@ F = [F, Q*((A.') + a*(b.')) + (A+b*(a.'))*Q - z*(b.') - b*(z.') < 0];
 F = [F, Q*N + N*Q < 0];
 
 % 4.62
-F = [F, [[1, (x.')]; [x, Q]] > 0];
+for i = 1: Xi_o_num
+  # F = [F, [[1, (x.')]; [x, Q]] > 0];
+  F = [F, [[1, Xi_o(i,:)]; [(Xi_o(i,:).'), Q]] > 0];
+end
+
 
 % 4.63
 #disp(size(u_max^2 -(a.')*Q*a + 2 .* (a.')*z));
@@ -67,31 +100,29 @@ F = [F, [[1, (x.')]; [x, Q]] > 0];
 #disp(size([(u_max^2 -(a.')*Q*a + 2 * (a.')*z), (z.')]));
 #disp(size( [ z, Q]));
 F = [F,  [[(u_max^2 -(a.')*Q*a + 2 * (a.')*z), (z.')]; [ z, Q]] >=0];
+
 % 4.64
 # i: belong to {0,1, ..., m}
 tmp = 0;
+disp ("Q = ");
+disp(Q);
+disp ("a = ");
+disp(a);
+disp ("I = ");
+disp(I);
+disp ("M = ");
+disp(M);
+disp ("N = ");
+disp(N);
+
+# m: size of a: dimention of A (nxn) ????
+m = 3;
 for i = 0 : m
-  tmp = tmp + sum(i,Q,a, I, M);
+  disp ("Looping...");
+  tmp = tmp + sum_func(i, Q, a, I, M, N);
 end
 
 F = [F, tmp >= 0];
-
-function retval = P (l, k, I, M)
-  retval = 1;
-  for q = (0: k-1)
-    retval = retval * ((l-q)*I + M);
-  endfor
-  return
-endfunction
-
-function retval = sum(i,Q,a, I, M)
-  retval = 1;
-  for k = (0: i)
-    retval = retval + nchoosek(i,k)*(a.')*P(0, i-k, I, M)*Q*N*P(0,k, I, M)*a
-                    -(z.')*N*P(n,i, I, M)*a;
-  endfor
-  return
-endfunction
 
 % ___________Define an objective
 objective = 0;
